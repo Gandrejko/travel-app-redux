db6f12175906fa5aec01915d829804ffb50bfdf0
Andrew <gandrejko2002@gmail.com>
2023-02-21 20:55:27 +0200

    add trip cards where each card is another component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import {Filter} from "./Filter";
+import {Trips} from "./Trips";
 
 export const Main = () => {
 	return (
 		<main>
 			<h1 className="visually-hidden">Travel App</h1>
 			<Filter />
+			<Trips />
 		</main>
 	)
 }
