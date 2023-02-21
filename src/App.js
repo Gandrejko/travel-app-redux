65bad17d61b49f9a19481b25e930b69e31577ede
Andrew <gandrejko2002@gmail.com>
2023-02-21 22:18:55 +0200

    installed React Router and added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+
+import {Routes, Route, Link} from "react-router-dom";
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
 import {Main} from "./components/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route path='/' element={<Main />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
