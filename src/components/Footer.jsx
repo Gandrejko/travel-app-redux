cdbe5321e185e847d428550a2218b5497bfa09bb
Andrew <gandrejko2002@gmail.com>
2023-02-21 20:27:03 +0200

    add footer to layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import heart from '../assets/images/heart.svg';
+export const Footer = () => {
+	return (
+		<div className='footer'>
+			<span className="footer__text">
+				from
+				<a className="footer__link" href="https://binary-studio.com">
+				  binary studio
+				</a>
+				with
+				<img
+					className="footer__icon"
+					src={heart}
+					alt="heart icon"
+				/>
+			</span>
+		</div>
+	)
+}
