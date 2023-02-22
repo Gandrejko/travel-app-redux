be340047dac867b531ffff21adc2fb5f5aefb2ac
Andrew <gandrejko2002@gmail.com>
2023-02-22 03:03:05 +0200

    add modal on trip page + validate inputs

--- src/App.js
+++ src/App.js
@@ -14,6 +14,26 @@ function App() {
 	const [trips, setTrips] = useState(tripsJSON);
 	const [bookings, setBookings] = useState(bookingsJSON);
 
+	const addBooking = (tripId) => {
+		setBookings(prevState => {
+			const tripDetails = trips.find(trip => trip.id === tripId);
+			/*const newBooking = {
+				id: "ee3be8e7-648f-4fce-bdb5-ba0c7cb38c55",
+				userId: "1dd97a12-848f-4a1d-8a7d-34a2132fca94",
+				tripId: "4c7564ad-fafc-4641-a692-55a683de7fbe",
+				guests: 1,
+				date: "2024-09-03T14:37:00.049Z",
+				trip: {
+					title: "Scotland",
+					duration: 8,
+					price: 2145
+				},
+				totalPrice: 2145,
+				createdAt: "2022-05-22T17:44:56.660Z"
+			};*/
+		})
+	}
+
 	const deleteBooking = (bookingId) => {
 		setBookings(bookings.filter(booking => booking.id !== bookingId))
 	}
