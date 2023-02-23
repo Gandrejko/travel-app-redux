d3dd3bf25c7cdd5a710912af6d9a578de4a687a4
Andrew <gandrejko2002@gmail.com>
2023-02-23 21:09:10 +0200

    change contains to search

--- src/functions/filterBySearch.js
+++ src/functions/filterBySearch.js
@@ -1,5 +1,5 @@
 export const filterBySearch = (trips, search) => {
   return trips.filter(
-    (trip) => trip.title.toLowerCase().contains(search.toLowerCase())
+    (trip) => trip.title.toLowerCase().search(search.toLowerCase()) > 0 // but contains is better)
   );
 };
